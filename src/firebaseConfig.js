5bf6058bdb508c462b39c92d1d765b17950db97b
sumedhx <sumedhsuralkar@gmail.com>
2025-02-16 03:53:27 +0530

    .

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,22 +4,23 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYVi7ORqiCFKf5-dz9n-ONJAHYOi3U9Cw",
-  authDomain: "simple-ticket-manager.firebaseapp.com",
-  projectId: "simple-ticket-manager",
-  storageBucket: "simple-ticket-manager.appspot.com",
-  messagingSenderId: "845296528005",
-  appId: "1:845296528005:web:4f16a11c54ec88e19a57ec",
-  measurementId: "G-6RM2DRHZ86"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app); 
+const storage = getStorage(app);
 
-export { app, auth, db , storage};
+export { app, auth, db, storage };
+
 
 
 
